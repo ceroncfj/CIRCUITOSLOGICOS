e5f578af61b3f5e468bca2d0e165ebb724d50544
Sergio Alberto Cuevas Cerón <61673382+ceroncfj@users.noreply.github.com>
2022-12-02 15:58:11 -0600

    p5

--- PRACTICA5/practica5.vhd
+++ PRACTICA5/practica5.vhd
@@ -1,23 +1,37 @@
+-- Práctica 5 señales 
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 use ieee.std_logic_unsigned.all;
+use ieee.std_logic_arith.all;
 
 entity practica5 is port(
-	clk: in std_logic;
-	outx: out std_logic_vector(7 downto 0)
+	clk50M: in std_logic;
+	clk: out std_logic
 );
 end practica5;
 
 architecture topLevel of practica5 is
 	signal contador: std_logic_vector(23 downto 0):=x"000000";
 begin
-	process(clk) begin
-	 if (rising_edge(clk)) then 
+	process(clk50M) begin
+	 if (rising_edge(clk50M)) then 
 		contador<=contador + 1;
-		if (contador<1000)then
-		
+		if (contador<x"002625A0")then
+			clk<= '1';
+		elsif(contador=x"004C4B40") then
+			contador<= x"000000";
+		else
+			clk<= '0';
+		end if;
 	 end if;
 	end process;
 end topLevel;
 
+
+-- Práctica 5 señales 
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+use ieee.std_logic_unsigned.all;
+use ieee.std_logic_arith.all;
